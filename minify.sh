7aac011f3c44b0563854db498a8e05718bb0d5ff
Henrique F. Teixeira <hriqueft@gmail.com>
2026-05-23 15:05:34 -0300

    update grid

--- minify.sh
+++ minify.sh
@@ -2,3 +2,7 @@ css-minify -f grid.css
 css-minify -f helpers.css
 css-minify -f shadow.css
 css-minify -f responsive-nav.css
+css-minify -f messages.css
+css-minify -f tables.css
+css-minify -f general.css
+css-minify -f buttons.css
